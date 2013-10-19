17646a914a6e527636a93119504a6777d736f4ae
Kir Shatrov <shatrov@me.com>
2013-10-19 12:24:06 +0200

    Don't update gem twice

--- app/workers/fetch_gem_worker.rb
+++ app/workers/fetch_gem_worker.rb
@@ -1,11 +1,21 @@
 class FetchGemWorker
   include Sidekiq::Worker
 
+  LATENCY = 3.hours.ago
+
   def perform(gem_name)
+    return if gem_up_to_date?(gem_name)
+
     sleep 10
 
     ruby_gem = RubyGem.create(name: gem_name)
 
     Version.create(ruby_gem: ruby_gem, number: "0.0.1", release_notes: "requested release...")
+  end
+
+  private
+
+  def gem_up_to_date?(gem_name)
+    RubyGem.where(name: gem_name).where("updated_at > ?", LATENCY).any?
   end
 end
