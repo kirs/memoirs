ed23055433de45b37a49604266c5e0f2018cf258
Kir Shatrov <shatrov@me.com>
2013-10-20 17:42:19 +0200

    Refactored JS

--- public/frontend/js/memoirs.js
+++ public/frontend/js/memoirs.js
@@ -16,49 +16,80 @@ $(function() {
     modalBlock.modal()
   }
 
-  displayVersion = function(gemName, versionNumber) {
-    if(!window.gems) {
-      $.getJSON("/ruby_gems/" + gemName + "/versions", function(gemVersions) {
-        var targetVersion = _.find(gemVersions, function(version) {
-          return version.number == versionNumber
-        })
-
-        targetVersion = $.extend(targetVersion, { gem: {name: "loaded"} })
-        openVersionModal(targetVersion)
-      })
-      return
-    }
-    var gem = _.find(window.gems, function(gem) {
-      return gemName == gem.name
-    })
-
-    var version = _.find(gem.versions, function(version) {
+  findVersionInCollection = function(versions, versionNumber) {
+    return _.find(versions, function(version) {
       return version.number == versionNumber
     })
-
-    version = $.extend(version, { gem: gem })
-
-    openVersionModal(version)
   }
 
-  $.getJSON("/ruby_gems", function(loadedGems) {
-    window.gems = loadedGems
-    $("#popular_gems_container").append(collectionTemplate(loadedGems))
-  })
+  Memoirs = {
+    stateChanged: function(event) {
+      hash = document.location.hash
 
-  $(window).on('popstate', function(event) {
-    hash = document.location.hash
+      if(result = hash.match(/#!\/([a-z]*)\/([\w.]*)/)) {
+        gemName = result[1]
+        versionNumber = result[2]
 
-    if(result = hash.match(/#!\/([a-z]*)\/([\w.]*)/)) {
-      gemName = result[1]
-      versionNumber = result[2]
+        Memoirs.displayVersion(gemName, versionNumber)
+      }
+    },
 
-      displayVersion(gemName, versionNumber)
+    displayVersion: function(gemName, versionNumber) {
+      if(!Memoirs.loadedGems) {
+        delayedLoad(gemName, versionNumber)
+        return
+      }
+
+      var gem = _.find(Memoirs.loadedGems, function(gem) {
+        return gemName == gem.name
+      })
+      version = findVersionInCollection(gem.versions, versionNumber)
+      // if version == undefined, show NOT FOUND
+
+      versionWithGem = $.extend(version, { gem: gem })
+
+      openVersionModal(versionWithGem)
+    },
+
+    fetchList: function(limit) {
+      url = "/ruby_gems?limit=" + limit
+      $.getJSON(url, function(gems) {
+        Memoirs.loadedGems = gems
+        $("#popular_gems_container").append(collectionTemplate(gems))
+      })
     }
-  })
+  }
+  delayedLoad = function(gemName, versionNumber) {
+    url = "/ruby_gems/" + gemName + "/versions?specific=" + versionNumber
+    $.getJSON(url, function(gemVersions) {
+      if(gemVersions.status == "processing") {
+        fakeVersion = {
+          release_notes: "Loading gem meta...",
+          gem: {
+            name: gemName
+          }
+        }
+        openVersionModal(fakeVersion)
 
+        Memoirs.asyncFetcher = setTimeout(function() {
+          delayedLoad(gemName, versionNumber)
+        }, 3000)
+      } else {
+        targetVersion = findVersionInCollection(gemVersions, versionNumber)
+
+        targetVersion = $.extend(targetVersion, { gem: {name: gemName} })
+        openVersionModal(targetVersion)
+      }
+    })
+  }
+
+  Memoirs.fetchList(10)
+
+  $(window).on('popstate', Memoirs.stateChanged)
+
+  // back on modal hide
   $('.modal').on('hidden.bs.modal', function () {
-    console.log("on hide")
+    window.clearInterval(Memoirs.asyncFetcher)
     history.pushState(null, null, "/frontend/")
   })
 })
