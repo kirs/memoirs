b6130476258f0f463c402b8bd251be00a52700cb
Kir Shatrov <shatrov@me.com>
2013-10-20 13:26:45 +0200

    Open version in modal

--- public/frontend/js/memoirs.js
+++ public/frontend/js/memoirs.js
@@ -1,15 +1,43 @@
 $(function() {
+  Handlebars.registerHelper( "join", function( array, sep, options ) {
+    return array.map(function( item ) {
+      return options.fn( item );
+    }).join( sep );
+  });
+
   var collectionTemplateScript = $("#gem_list")
-  var versionsTemplateScript = $("#version_list")
+  var versionTemplateScript = $("#version_modal")
 
   if(collectionTemplateScript.length > 0) {
     var collectionTemplate = Handlebars.compile(collectionTemplateScript.html())
+    var versionTemplate = Handlebars.compile(versionTemplateScript.html())
+
     $.getJSON("/ruby_gems", function(gems) {
+      window.gems = gems
       $("#popular_gems_container").append(collectionTemplate(gems))
     })
 
     $(document).on("click", ".open-version", function() {
-      alert("open " + $(this).text())
+      $(".modal").remove() // cleanup
+
+      targerGemName = $(this).data('gem-name')
+      targetVersion = $(this).data('version-number')
+
+      var gem = _.find(window.gems, function(gem) {
+        return targerGemName == gem.name
+      })
+
+      var version = _.find(gem.versions, function(version) {
+        return version.number == targetVersion
+      })
+
+      version = $.extend(version, { gem: gem })
+
+      var modal = versionTemplate(version)
+      modal = $(modal)
+
+      $("body").append(modal)
+      $("body .modal").modal()
     })
   }
 })
