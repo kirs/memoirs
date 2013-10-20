214bb7b7662932e7985e9c217c819036c17085e2
Andrey Ognevsky <a.ognevsky@gmail.com>
2013-10-20 18:26:32 +0200

    Create gem versions after parse

--- app/models/release_notes_importer.rb
+++ app/models/release_notes_importer.rb
@@ -12,7 +12,11 @@ module ReleaseNotesImporter
   private
 
   def self.create_needed_versions(versions, file_extension, gem_name)
-    # TODO create versions here
-    puts [versions, file_extension, gem_name].inspect
+    gem = RubyGem.first_or_create(name: gem_name)
+    gem.versions.destroy_all
+
+    versions.each do |version_number, release_notes|
+      gem.versions.create!(number: version_number, release_notes: release_notes)
+    end
   end
 end
